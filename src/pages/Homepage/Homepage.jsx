b98831723aa5cea79d4eb01d4ecf28f969e5b7a5
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-07 23:21:49 +0900

    메인화면 배너 추가

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "./components/Banner/Banner";
+
+// 1. 배너 => popular movie 들고와서 첫번째 아이템을 보여주자
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
+
+const Homepage = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default Homepage;
