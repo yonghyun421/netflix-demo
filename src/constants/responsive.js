a47253c97abe9e89fc5766a0a9e918ae332093ac
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-10 12:47:21 +0900

    모바일 버젼 수정

--- src/constants/responsive.js
+++ src/constants/responsive.js
@@ -1,14 +1,15 @@
 export const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 6,
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 2,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 664, min: 0 },
     items: 1,
+    partialVisibilityGutter: 30,
   },
 };
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./components/Banner/Banner";
 import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 import TopRatedMovieSlide from "./components/TopRatedMovieSlide/TopRatedMovieSlide";
@@ -10,12 +10,30 @@ import UpcomingMovieSlide from "./components/UpcomingMovieSlide/UpcomingMovieSli
 // 4. upcoming movie
 
 const Homepage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth <= 768); // You can adjust the threshold as needed
+    };
+
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
   return (
     <div>
       <Banner />
-      <PopularMovieSlide />
-      <TopRatedMovieSlide />
-      <UpcomingMovieSlide />
+      <div className={isMobile ? "px-3" : "px-5"}>
+        <PopularMovieSlide />
+        <TopRatedMovieSlide />
+        <UpcomingMovieSlide />
+      </div>
     </div>
   );
 };
